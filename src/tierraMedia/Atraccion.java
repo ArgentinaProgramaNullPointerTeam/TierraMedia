6012dc1990f47ab23e9e50994817097d0a4769a1
CaroRuvira <caroruvira@gmail.com>
2021-09-02 19:09:03 -0300

    Se agrega el desarrollo inicial de la clase Atraccion

--- src/tierraMedia/Atraccion.java
+++ src/tierraMedia/Atraccion.java
@@ -1,5 +1,38 @@
 package tierraMedia;
 
 public class Atraccion {
+	private String nombre;
+	private int costoDeVisita;
+	private double tiempoDeVisita;
+	private int cupo;
+	private TipoAtraccion tipoAtraccion;
+
+	public Atraccion(String nombre, int costoDeVisita, double tiempoDeVisita, int cupo, TipoAtraccion tipoAtraccion) {
+		this.nombre = nombre;
+		this.costoDeVisita = costoDeVisita;
+		this.tiempoDeVisita = tiempoDeVisita;
+		this.cupo = cupo;
+		this.tipoAtraccion = tipoAtraccion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public int getCostoDeVisita() {
+		return costoDeVisita;
+	}
+
+	public double getTiempoDeVisita() {
+		return tiempoDeVisita;
+	}
+
+	public int getCupo() {
+		return cupo;
+	}
+
+	public TipoAtraccion getTipoAtraccion() {
+		return tipoAtraccion;
+	}
 
 }
