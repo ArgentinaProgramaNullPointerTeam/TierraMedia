2525da114475b5415a4e1e79a18625f100aaf294
vivi-ana <acostaviviana.a@gmail.com>
2021-09-04 18:57:06 -0300

    Se agregan métodos a la clase Usuario

--- src/tierraMedia/Usuario.java
+++ src/tierraMedia/Usuario.java
@@ -6,9 +6,12 @@ import java.util.List;
  * Clase que modela al usuario
  * Tiene un constructor con los
  * @param nombre, atraccionPreferida, dineroDisponible, tiempoDisponible
- * Tiene los getter para tiempoDisponible, dineroDisponible y listaCompra
+ * Tiene los getter para tiempoDisponible, dineroDisponible, atraccionPreferida
+ *  y listaCompra
  * Tiene un metodo guardarSugerencia que recibe un producto y 
  * lo guarda en listaCompra 
+ * Tiene un metodo restarTiempo y restarDinero que resta el tiempo y el dinero
+ * respectivamente.
  */
 public class Usuario {
 	private String nombre;
@@ -32,11 +35,39 @@ public class Usuario {
 	public double getDineroDisponible() {
 		return dineroDisponible;
 	}
-
+	
+	public TipoAtraccion getAtraccionPreferida() {
+		return atraccionPreferida;
+	}
+	
 	public List<Producto> getListaCompra() {
 		return listaCompra;
 	}
+	
 	public void guardarSugerencia(Producto producto) {
 		listaCompra.add(producto);
 	}
+	
+	public void restarDinero(double dinero) throws Exception {
+		if (this.dineroDisponible>=dinero) {
+		this.dineroDisponible -= dinero;
+		}else {
+			throw new Exception ("Dinero insuficiente");
+		}
+	}
+	
+	public void restarTiempo(double tiempo) throws Exception {
+		if(this.tiempoDisponible >= tiempo) {
+			this.tiempoDisponible -= tiempo;
+		}else {
+			throw new Exception ("Tiempo insuficiente");
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Nombre de usuario= " + nombre + ", Atraccion preferida= " + atraccionPreferida + ", Dinero disponible= "
+				+ dineroDisponible + ", Tiempo disponible= " + tiempoDisponible;
+	}
+	
 }
