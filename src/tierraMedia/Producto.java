85b0ddb3df58191fe9b8225d0b11156abdc72bf9
PatNatMar <87343240+PatNatMar@users.noreply.github.com>
2021-09-03 21:38:00 -0300

    Create Producto.java
    Reponer clase Producto eliminada

---
+++ src/tierraMedia/Producto.java
@@ -0,0 +1,30 @@
+package tierraMedia;
+
+public class Producto {
+	
+	//Devuelve si hay cupos
+	public boolean hayCupo() {
+		return true;
+	}
+	
+	//Devuelve el costo	
+	public int getCosto() {
+		return costo;
+	}
+	
+	//Devuelve el tipo
+	public String getTipo() {
+		return tipo;	
+	}
+	
+	//Devuelve el tiempo
+	public int getTiempo() {
+		return tiempo;
+	}
+	
+	
+	
+	
+	
+
+}
