7e8f7feca5c2e2e5318ff293bd1d55ffe2cbcd17
vivi-ana <acostaviviana.a@gmail.com>
2021-09-05 15:02:51 -0300

    Modificación del método agregarSugerencia

--- src/tierraMedia/Usuario.java
+++ src/tierraMedia/Usuario.java
@@ -2,16 +2,15 @@ package tierraMedia;
 
 import java.util.ArrayList;
 import java.util.List;
+
 /**
- * Clase que modela al usuario
- * Tiene un constructor con los
- * @param nombre, atraccionPreferida, dineroDisponible, tiempoDisponible
- * Tiene los getter para tiempoDisponible, dineroDisponible, atraccionPreferida
- *  y listaCompra
- * Tiene un metodo guardarSugerencia que recibe un producto y 
- * lo guarda en listaCompra 
- * Tiene un metodo restarTiempo y restarDinero que resta el tiempo y el dinero
- * respectivamente.
+ * Clase que modela al usuario Tiene un constructor con los
+ * @param nombre, atraccionPreferida, dineroDisponible, tiempoDisponible Tiene
+ * los getter para tiempoDisponible, dineroDisponible,
+ * atraccionPreferida y listaCompra Tiene un metodo
+ * guardarSugerencia que recibe un producto y lo guarda en
+ * listaCompra Tiene un metodo restarTiempo y restarDinero que
+ * resta el tiempo y el dinero respectivamente.
  */
 public class Usuario {
 	private String nombre;
@@ -19,7 +18,7 @@ public class Usuario {
 	private double dineroDisponible;
 	private double tiempoDisponible;
 	private List<Producto> listaCompra = new ArrayList<Producto>();
-	
+
 	public Usuario(String nombre, TipoAtraccion atraccionPreferida, 
 			double dineroDisponible, double tiempoDisponible) {
 		this.nombre = nombre;
@@ -35,39 +34,42 @@ public class Usuario {
 	public double getDineroDisponible() {
 		return dineroDisponible;
 	}
-	
+
 	public TipoAtraccion getAtraccionPreferida() {
 		return atraccionPreferida;
 	}
-	
+
 	public List<Producto> getListaCompra() {
 		return listaCompra;
 	}
-	
-	public void guardarSugerencia(Producto producto) {
+
+	public void guardarSugerencia(Producto producto) throws Exception {
+		this.restarDinero(producto.getCostoDeVisita());
+		this.restarTiempo(producto.getTiempoDeVisita());
 		listaCompra.add(producto);
 	}
-	
+
 	public void restarDinero(double dinero) throws Exception {
-		if (this.dineroDisponible>=dinero) {
-		this.dineroDisponible -= dinero;
-		}else {
-			throw new Exception ("Dinero insuficiente");
+		if (this.dineroDisponible >= dinero) {
+			this.dineroDisponible -= dinero;
+		} else {
+			throw new Exception("Dinero insuficiente");
 		}
 	}
-	
+
 	public void restarTiempo(double tiempo) throws Exception {
-		if(this.tiempoDisponible >= tiempo) {
+		if (this.tiempoDisponible >= tiempo) {
 			this.tiempoDisponible -= tiempo;
-		}else {
-			throw new Exception ("Tiempo insuficiente");
+		} else {
+			throw new Exception("Tiempo insuficiente");
 		}
 	}
 
 	@Override
 	public String toString() {
-		return "Nombre de usuario= " + nombre + ", Atraccion preferida= " + atraccionPreferida + ", Dinero disponible= "
+		return "Nombre de usuario= " + nombre + ", Atraccion preferida= " 
+				+ atraccionPreferida + ", Dinero disponible= "
 				+ dineroDisponible + ", Tiempo disponible= " + tiempoDisponible;
 	}
-	
+
 }
