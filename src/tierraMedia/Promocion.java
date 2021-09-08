72d0b6d7ab079c2e1778b1d8e1b5a525cfb1d18c
PatNatMar <87343240+PatNatMar@users.noreply.github.com>
2021-09-08 09:38:49 -0300

    Update Promocion.java
    Atributos y constructor segun los datos que se reciben por archivo de Promociones...

--- src/tierraMedia/Promocion.java
+++ src/tierraMedia/Promocion.java
@@ -1,5 +1,20 @@
 package tierraMedia;
 
 public class Promocion {
-
+	private static String nombre;
+	private static int cantAtracciones;
+	private static List<Atraccion> atracciones;
+	private static String tipoPromocion;
+	private static String descuento;
+	
+	public void Promocion (String nombre, int cantAtracciones, List<Atraccion> atracciones, String tipoPromocion, String descuento) {
+		this.nombre = nombre;
+		this.cantAtracciones = cantAtracciones;
+		this.atracciones= atracciones;
+		this.tipoPromocion = tipoPromocion;
+		this.descuento= descuento;	
+	}
+	
+	
+	
 }
