216ff350de639c00e68e43df8784e5a27367a04b
PatNatMar <87343240+PatNatMar@users.noreply.github.com>
2021-09-10 16:23:05 -0300

    Update ComparadorDeProductos.java
    correccion de condiciones equals

--- src/tierraMedia/ComparadorDeProductos.java
+++ src/tierraMedia/ComparadorDeProductos.java
@@ -1,5 +1,7 @@
 package tierraMedia;
 
+import java.util.Comparator;
+
 public class ComparadorDeProductos implements Comparator<Producto> {
 	// Atributo para guardar la atraccion preferida del usuario
 	private TipoAtraccion atraccionPreferida;
@@ -10,38 +12,32 @@ public class ComparadorDeProductos implements Comparator<Producto> {
 	}
 
 	@Override
-		public int compare(Producto p1, Producto p2) {
-			/*Metodo que ordena segun prioridades:
-			 * 1 Coincida con la atraccion preferida del usuario 
-             /////ver costo y tiempo (o en App)
-			 * 2 Promocion por sobre atraccion
-			 * 3 Mayor costo 
-			 * 4 Mayor tiempo
-			 */
-			if(this.atraccionPreferida== p1.getTipo() 
-					&& this.atraccionPreferida !=p2.getTipo())
+	public int compare(Producto p1, Producto p2) {
+		/*
+		 * Metodo que ordena segun prioridades: 1 Coincida con la atraccion preferida
+		 * del usuario /////ver costo y tiempo (o en App) 2 Promocion por sobre
+		 * atraccion 3 Mayor costo 4 Mayor tiempo
+		 */
+		if (this.atraccionPreferida == p1.getTipo() && this.atraccionPreferida != p2.getTipo())
+			return -1;
+		else if (this.atraccionPreferida != p1.getTipo() && this.atraccionPreferida == p2.getTipo())
+			return 1;
+		else {
+			if (p1.getClass().getName().equals("Promocion"))
 				return -1;
-			else if (this.atraccionPreferida!= p1.getTipo() 
-					&& this.atraccionPreferida == p2.getTipo())
+			else if (p1.getClass().getName().equals("Atraccion"))
 				return 1;
-			else{ 
-				if (p1.getClass().getName() == "Promocion" &&
-						&& p2.getClass().getName()!= "Atraccion") 
+			else {
+				if (p1.getCostoDeVisita() > p2.getCostoDeVisita())
 					return -1;
-				else if (p1.getClass().getName() == "Atraccion" &&
-						&& = p2.getClass().getName()!= "Promocion")
+				else if (p1.getCostoDeVisita() < p2.getCostoDeVisita())
 					return 1;
 				else {
-					if (p1.getCostoDeVisita()> p2.getCostoDeVisita()) 
-						return -1;
-					else if (p1.getCostoDeVisita()< p2.getCostoDeVisita())
-						return 1;
-					else {
-						Double.compare(p1.getTiempoDeVisita(), p2.getTiempoDeVisita());
-					}
+					return Double.compare(p1.getTiempoDeVisita(), p2.getTiempoDeVisita());
 				}
-						
 			}
+
+		}
 	}
 
 }
