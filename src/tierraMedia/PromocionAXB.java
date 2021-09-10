d211235b10e7f299d4f07c11453b753dad908c48
PatNatMar <87343240+PatNatMar@users.noreply.github.com>
2021-09-10 16:25:30 -0300

    Update PromocionAXB.java
    Se agrega constructor y metodo que devuelve el costo total con el descuento de la atraccion gratuita

--- src/tierraMedia/PromocionAXB.java
+++ src/tierraMedia/PromocionAXB.java
@@ -1,9 +1,35 @@
 package tierraMedia;
 
+import java.util.List;
+
 public class PromocionAXB extends Promocion{
 	
+	public PromocionAXB(String nombre, int cantAtracciones, List<Atraccion> atracciones, String tipoPromocion,
+			String descuento) {
+		super(nombre, cantAtracciones, atracciones, tipoPromocion, descuento);
+	}
+	
+	private String nombre;
+	private int cantAtracciones;
+	private  List<Atraccion> atracciones;
+	private  String tipoPromocion;
+	private  String descuento;
+	
+
+
+	public int costoConAtraccionDescontada(List<Atraccion> atracciones) {
+		int costoTotal=0;
+		Atraccion atraccionGratuita=atracciones.get(atracciones.size() - 1);
+		for(Atraccion cadaAtraccion: atracciones) {
+			costoTotal= costoTotal + cadaAtraccion.getCostoDeVisita();
+		}
+		costoTotal=costoTotal-atraccionGratuita.getCostoDeVisita();   
+		return costoTotal;
+	}
+	
+	
+}
 	
 
 	
 	
-}
