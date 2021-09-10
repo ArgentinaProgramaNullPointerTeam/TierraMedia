a257e743bb604ec0ceb5e6b851c587e0a3f68df1
PatNatMar <87343240+PatNatMar@users.noreply.github.com>
2021-09-10 16:26:40 -0300

    Update PromocionPorcentual.java
    Se agrega constructor y metodo que devuelve el costo total con el porcentaje de descuento aplicado

--- src/tierraMedia/PromocionPorcentual.java
+++ src/tierraMedia/PromocionPorcentual.java
@@ -1,11 +1,27 @@
 package tierraMedia;
 
-public class PromocionPorcentual extends Promocion {
+import java.util.List;
 
-	public int costoConPorcentajedescontado(List<Atraccion> atracciones, double porcentaje){
+public class PromocionPorcentual extends Promocion {
+	private static String nombre;
+	private static int cantAtracciones;
+	private static List<Atraccion> atracciones;
+	private static String tipoPromocion;
+	private static Double descuento;
+	
+	public PromocionPorcentual(String nombre, int cantAtracciones, List<Atraccion> atracciones, String tipoPromocion,
+			String descuento) {
+		super(nombre, cantAtracciones, atracciones, tipoPromocion, descuento);
+		
+	}
+
+	public int costoConPorcentajeDescontado(List<Atraccion> atracciones, String descuento) {
 	    int costoTotal=0;
+	    PromocionPorcentual.descuento=Double.parseDouble(descuento);
 		for(Atraccion atraccion: atracciones) {
 			costoTotal+= atraccion.getCostoDeVisita();
 		}
+		costoTotal=(int) (costoTotal-(costoTotal*PromocionPorcentual.descuento));
+		return costoTotal;
 	}
 }
