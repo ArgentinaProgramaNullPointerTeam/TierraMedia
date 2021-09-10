e2099224e8ba37c27bf414abb3ab27d3ebd6e160
PatNatMar <87343240+PatNatMar@users.noreply.github.com>
2021-09-10 16:24:50 -0300

    Update PromocionAbsoluta.java
    Se agrega constructor y metodo que devuelve el costo con el valor absoluto establecido por descuento

--- src/tierraMedia/PromocionAbsoluta.java
+++ src/tierraMedia/PromocionAbsoluta.java
@@ -1,6 +1,24 @@
 package tierraMedia;
 
+import java.util.List;
+
 public class PromocionAbsoluta extends Promocion{
+	private static String nombre;
+	private static int cantAtracciones;
+	private static List<Atraccion> atracciones;
+	private static String tipoPromocion;
+	private static Double descuento;
 	
-	
+	public PromocionAbsoluta(String nombre, int cantAtracciones, List<Atraccion> atracciones, String tipoPromocion,
+			String descuento) {
+		super(nombre, cantAtracciones, atracciones, tipoPromocion, descuento);
+		
+	}
+
+	public int costoConValorActualizado(List<Atraccion> atracciones, String descuento) {
+	    int costoTotal=Integer.parseInt(descuento);
+		return costoTotal;
+	}
 }
+	
+
