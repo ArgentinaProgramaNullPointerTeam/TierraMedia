cc0a38eea764e4aad12e5e047e7be65e13c2348e
PatNatMar <87343240+PatNatMar@users.noreply.github.com>
2021-09-15 16:46:38 -0300

    Update App.java
    App completa, falta mejorarla y probar la comparacion producto-listacompra

--- src/tierraMedia/App.java
+++ src/tierraMedia/App.java
@@ -1,18 +1,28 @@
 package tierraMedia;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
 	private static List<Usuario> usuarios = new ArrayList<Usuario>();
 	private static List<Producto> productos = new ArrayList<Producto>();
-
+	private static String entradaUsuario="";
+	
+	private static String saludoBienvenida(String nombreUsuario) {
+		return "Hola " + nombreUsuario;
+	}
+	
+	private static String saludoDespedida(String nombreUsuario) {
+		return "Que disfrute de su compra " + nombreUsuario;
+	}
 	public static void main(String[] args) {
 		List<Atraccion> atracciones;
 		List<Promocion> promociones;
 
+		
 		System.out.println("Leer\n");
 		usuarios = AdministradorDeArchivos.leerUsuarios(); // Tiene q devolver un array de usuarios
 
@@ -28,25 +38,45 @@ public class App {
 
 		System.out.println(usuarios);
 
-		// ordenar sugerencia
-		for (Usuario cadaUsuario : usuarios) {
-			System.out.println("antes de ordenar" + productos);
-			System.out.println("clase: "+ productos.get(0).getClass().getName());
-			Collections.sort(productos, new ComparadorDeProductos(cadaUsuario.getAtraccionPreferida()));
-			System.out.println("despues de ordenar" + productos);
-			
-			System.out.println(cadaUsuario.getNombre() +"--" + (productos.get(0).getNombre()));
 		
+		
+		Scanner entradaDeUsuario=new Scanner(System.in);
+		
+		
+		for (Usuario cadaUsuario : usuarios) {
+			Collections.sort(productos, new ComparadorDeProductos(cadaUsuario.getAtraccionPreferida()));
+			System.out.println(saludoBienvenida(cadaUsuario.getNombre()));
+			for(Producto cadaProducto: productos) {
+				if(cadaUsuario.puedeComprar(cadaProducto) &&!cadaProducto.fueComprado(cadaUsuario.getListaCompra())) {  
+					System.out.println(cadaProducto.ofertas());
+					System.out.println("Si desea adquirir este producto ingrese SI, de lo contrario ingrese NO");
+				    entradaUsuario= entradaDeUsuario.nextLine();
+				    if(entradaUsuario.contains("SI")){
+				    	cadaUsuario.comprar(cadaProducto);
+				    	System.out.println(cadaProducto.getNombre() + " fue agregado a su itinerario.");
+				    }
+				    
+				    entradaUsuario="";
+				}else if(!cadaUsuario.puedeComprar(cadaProducto)) {
+					System.out.println("No puede adquirir: " +cadaProducto.getNombre() +" porque no posee dinero o tiempo suficiente");
+				}else if(cadaProducto.fueComprado(cadaUsuario.getListaCompra())){
+					System.out.println("No puede adquirir: " + cadaProducto.getNombre() + " porque la/las atracciones ya fueron adquiridas anteriormente");
+				}
+			}
+			
+			System.out.println(saludoDespedida(cadaUsuario.getNombre()));
+		    
 		}
+		entradaDeUsuario.close();
+		
+	     for(Usuario cadaUsuario: usuarios) {
+	    	 String listaCompra= cadaUsuario.getListaCompra().toString();
+	    	 ArrayList<String> datos = new ArrayList<String>();
+	 		datos.add(listaCompra);
+	 		AdministradorDeArchivos.guardarArchivo(datos, "prueba");
+	     }
 
-		/*
-		 * for(Promocion cadaPromo: promociones) {
-		 * System.out.println(cadaPromo.getCostoDeVisita()); }
-		 */
-
-		ArrayList<String> datos = new ArrayList<String>();
-		datos.add("algo");
-		AdministradorDeArchivos.guardarArchivo(datos, "prueba");
+		
 	}
 
 }
