34d7cd9c4c4eaa35917b6842aaa7fd0e5dba07ad
PatNatMar <87343240+PatNatMar@users.noreply.github.com>
2021-09-15 19:36:48 -0300

    Test
    Agregar

---
+++ tests/tierraMedia/ComparadorDeProductosTest.java
@@ -0,0 +1,14 @@
+package tierraMedia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComparadorDeProductosTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ tests/tierraMedia/PromocionAbsolutaTest.java
@@ -0,0 +1,14 @@
+package tierraMedia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PromocionAbsolutaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
